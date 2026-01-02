752d6dbccf72d1d37c5128b89a4defcc98162be8
siradjevaga-ios <siradjevaga@gmail.com>
2026-01-02 13:03:35 +0400

    Initial Commit

---
+++ MidTermProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MidTermProject
+//
+//  Created by user on 02.01.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
