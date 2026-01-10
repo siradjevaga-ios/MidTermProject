ad2b4d5c4368b40ecb67e6d5bf88f719fcb11303
siradjevaga-ios <siradjevaga@gmail.com>
2026-01-10 14:01:32 +0400

    update

--- MidTermProject/CoreData/WishEntity+CoreDataProperties.swift
+++ MidTermProject/CoreData/WishEntity+CoreDataProperties.swift
@@ -24,5 +24,5 @@ extension WishEntity {
 }
 
 extension WishEntity : Identifiable {
-
+    
 }
---
+++ MidTermProject/CoreData/WishListViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  WishListViewModel.swift
+//  MidTermProject
+//
+//  Created by user on 10.01.26.
+//
+
+import UIKit
+import CoreData
+
+
+
+class WishListViewModel {
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var items: [WishEntity] = []
+    
+    
+    private func fetchItems() {
+        do {
+            items = try context.fetch(WishEntity.fetchRequest())
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func addItems(productId: Int64, userId: Int64) {
+        let item = WishEntity(context: context)
+        item.productId = productId
+        item.userId = userId
+        
+        do {
+            try context.save()
+            items.append(item)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+}
